7ce0779e46a269b3662f38a353ce6b7b82138199
Chengjie Yin <yinchengjie2@gmail.com>
2015-02-17 22:33:10 -0500

    merged my minor changes to fix UTF8 issue

--- assignment2.rb
+++ assignment2.rb
@@ -1,6 +1,7 @@
 email_address_pattern = /[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*@((?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)/
+rjust_num = 13
 
-words = IO.read("assignment_two_text.txt").split(/\s+/)
+words = File.readlines('assignment_two_text.txt').to_s.split(/\s+/)
 
 domain_frequency = Hash.new(0)
 words.each do |word|
@@ -10,5 +11,7 @@ words.each do |word|
     domain_frequency[domain.downcase] += 1
   end
 end
-sorted_domain_frequency = domain_frequency.sort_by { |key, value| key }
-sorted_domain_frequency.each { |key, value| puts "#{key}:".rjust(20) + "#{value} time(s)".rjust(12) }
+
+domain_frequency.sort_by { |key, value| key }.each do |domain|
+  puts "#{domain[0]}: ".rjust(rjust_num) + "#{domain[1]} time(s)".rjust(rjust_num)
+end
