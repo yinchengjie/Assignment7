216cb3307bdc6a3b92067e73471ccfb2be03ff5b
Chengjie Yin <yinchengjie2@gmail.com>
2015-04-03 23:43:18 -0400

    added test for remaining models

--- assignment6/sandbox/assignment.rb
+++ assignment6/sandbox/assignment.rb
@@ -17,3 +17,15 @@ puts Account.create(gender: "female", age: 120, first_name: "Sabrina", last_name
 
 # test gender text
 puts Account.create(gender: "balbla", age: 21, first_name: "Sabrina", last_name: "Richie").errors.full_messages
+
+# test relationship between User and Account
+User.first.account = Account.first
+puts User.first.account.inspect
+
+# test relationship between TodoItems and Tag
+TodoItem.first.tags << Tag.first
+puts Tag.first.todo_items.inspect
+
+Tag.second.todo_items << TodoItem.second
+puts Tag.second.todo_items.inspect
+
