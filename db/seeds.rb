24534551cb2cd08a0a891275a980d2a9e7e35fb5
Victor Yu <vcyu08@gmail.com>
2015-04-14 22:30:59 -0400

    Cleaned up seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,11 +18,6 @@ Account.create [
                    { gender: "male", age: 36, first_name: "Tim", last_name: "Smith" },
                ]
 
-TodoList.destroy_all
-TodoList.create [
-                    { list_name: "Business trip to New York City", list_due_date: "2015-09-23" },
-                    { list_name: "Vacation to Disney Land", list_due_date: "2015-07-01" },
-                ]
 matt  = User.find_by(login: "Matt")
 tim = User.find_by(login: "Tim")
 account_matt = Account.find_by(first_name: "Matt")
@@ -35,22 +30,3 @@ tim.account = account_tim
   matt.todo_lists << todolist
 end
 
-
-
-TodoItem.destroy_all
-TodoItem.create [
-                    { due_date: "2015-04-15", task_title: "Buy tickets", description: "Buy tickets to Disney Land online" },
-                    { due_date: "2015-05-01", task_title: "Reserve hotel", description: "Reserve hotel for two adults and two children" },
-                    { due_date: "2015-06-01", task_title: "Bring the car to mechanic", description: "Oil change and maintenance" },
-                    { due_date: "2015-03-28", task_title: "Buy plane tickets", description: "Buy round-trip plane tickets online" },
-                    { due_date: "2015-04-04", task_title: "Reserve hotel", description: "Reserve hotel for one adult" },
-                    { due_date: "2015-04-05", task_title: "Buy Broadway show ticket", description: "Buy Broadway show ticket online"},
-                ]
-
-Tag.destroy_all
-Tag.create [
-               { tag_name: "vacation" },
-               { tag_name: "business trip" },
-               { tag_name: "entertainment" },
-               { tag_name: "hotel" },
-           ]
