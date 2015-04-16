e94eacc5c406d59fcd5a282af2d2c862db0bcda9
Victor Yu <vcyu08@gmail.com>
2015-04-15 23:44:39 -0400

    Added todo_list complete/incomplete indicator

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def due_date(todo_list, attributes = {})
+
+    if todo_list.all_done?
+      attributes[:style] = "background: greenyellow; padding: 10px; border-radius: 15px; font-size: 1.7em;"
+    else
+      attributes[:style] = "background: red; padding: 10px; border-radius: 15px; font-size: 1.7em;"
+    end
+    content_tag(:span, "#{todo_list.list_due_date}", attributes)
+  end
 end
--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -5,4 +5,15 @@ class TodoList < ActiveRecord::Base
   validates :list_name, presence: true
 
   default_scope { order list_due_date: :asc }
+
+  def all_done?
+    all_done = true
+    todo_items.each do |todo_item|
+      if !todo_item.done
+        all_done = false
+        break
+      end
+    end
+    all_done
+  end
 end
