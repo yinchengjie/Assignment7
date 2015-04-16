7a1ac11a5df0ff457da7bbc2943bfa4c029f00c0
Victor Yu <vcyu08@gmail.com>
2015-04-16 00:17:02 -0400

    Cleaned up todo_lists_helper.rb

--- app/helpers/todo_lists_helper.rb
+++ app/helpers/todo_lists_helper.rb
@@ -1,10 +1,2 @@
 module TodoListsHelper
-  def list_due_date(condition, attributes = {})
-    if condition
-      attributes[:style] = "background: greenyellow; padding: 10px; border-radius: 15px; font-size: 1.7em;"
-    else
-      attributes[:style] = "background: red; padding: 10px; border-radius: 15px; font-size: 1.7em;"
-    end
-    content_tag(:span, "#{@todo_list.list_due_date}", attributes)
-  end
 end
